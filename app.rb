edf38613f5eeb532e891e6ac8d9698fadc330131
Krysta05 <martinezkrysta7@gmail.com>
2018-06-27 14:53:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,5 +14,15 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
-
+get '/medicines/:id' do
+@medicines = all_the_medicines.select do 
+  medicines.id == params[:id]
+ end.first
+  erb :'medicines/show.html'
 end
+
+ get "/Goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}!"
+  end
+  
